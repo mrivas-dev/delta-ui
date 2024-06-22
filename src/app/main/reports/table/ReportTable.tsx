bde8b19fcfc15d8f018113849f0dd4a9f97480d4
Matt Rivas <mrivas@mrivas.dev>
2024-06-22 16:42:42 -0300

    Added config file

--- src/app/main/reports/table/ReportTable.tsx
+++ src/app/main/reports/table/ReportTable.tsx
@@ -5,7 +5,7 @@ import {
 	useMaterialReactTable,
 	type MRT_ColumnDef
 } from 'material-react-table';
-import FuseLoading from '@fuse/core/FuseLoading';
+import tableConfig from "./ReportTableConfig";
 import { ReportTableColumns } from './ReportTableColumns';
 
 const ReportTable = ({ studies, isLoading }) => {
@@ -15,61 +15,17 @@ const ReportTable = ({ studies, isLoading }) => {
 		[]
 	);
 
-	const table = useMaterialReactTable({
-		columns,
-		layoutMode: 'semantic',
-		data: studies?.length ? studies : [],
-		enableColumnResizing: true,
-		enableRowSelection: true,
-		enableColumnFilters: false,
-		enableGlobalFilter: false, 
-		enableFullScreenToggle: false,
-		enableDensityToggle: false,
-		enableColumnOrdering: true,
-		initialState: {
-			pagination: { pageSize: 25, pageIndex: 0 }
-		},
-		muiPaginationProps: {
-			rowsPerPageOptions: [25, 50, 100],
-			variant: 'outlined',
-			color: 'primary',
-			shape: 'rounded',
-		},
-		renderEmptyRowsFallback: ({ table }) => (
-			<span>No data found (TODO)</span>
-		),
-		paginationDisplayMode: 'pages',
-		state: {
-			isLoading
-		},
-		muiTopToolbarProps: {
-			sx: {
-				minHeight: '5.5rem',
-				borderRadius: '16px',
-			}
-		},
-		muiBottomToolbarProps: {
-			sx: {
-				minHeight: '5.5rem',
-				borderRadius: '0 0 16px',
-			}
-		},
-		muiTableContainerProps: {
-			sx: {
-				overflow: 'hidden'
+	const table = useMaterialReactTable(
+		{
+			columns,
+			data: studies?.length ? studies : [],
+			state: {
+				isLoading
 			},
-		},
-		muiTablePaperProps: {
-			sx: {
-				padding: '16px',
-				marginTop: '16px',
-				borderRadius: '16px',
-			},
-		},
-		mrtTheme: (theme) => ({
-			baseBackgroundColor: '#fff',
-		}),
-	});
+			...tableConfig
+		}
+	);
+
 	return (
 		<MaterialReactTable table={table} />
 	);
---
+++ src/app/main/reports/table/ReportTableConfig.tsx
@@ -0,0 +1,54 @@
+
+const tableConfig: any = {
+    layoutMode: 'semantic',
+    enableColumnResizing: true,
+    enableRowSelection: true,
+    enableColumnFilters: false,
+    enableGlobalFilter: false,
+    enableFullScreenToggle: false,
+    enableDensityToggle: false,
+    enableColumnOrdering: true,
+    initialState: {
+        pagination: { pageSize: 25, pageIndex: 0 }
+    },
+    muiPaginationProps: {
+        rowsPerPageOptions: [25, 50, 100],
+        variant: 'outlined',
+        color: 'primary',
+        shape: 'rounded',
+    },
+    renderEmptyRowsFallback: ({ table }) => (
+        <span>No data found (TODO)</span>
+    ),
+    paginationDisplayMode: 'pages',
+
+    muiTopToolbarProps: {
+        sx: {
+            minHeight: '5.5rem',
+            borderRadius: '16px',
+        }
+    },
+    muiBottomToolbarProps: {
+        sx: {
+            minHeight: '5.5rem',
+            borderRadius: '0 0 16px',
+        }
+    },
+    muiTableContainerProps: {
+        sx: {
+            overflow: 'hidden'
+        },
+    },
+    muiTablePaperProps: {
+        sx: {
+            padding: '16px',
+            marginTop: '16px',
+            borderRadius: '16px',
+        },
+    },
+    mrtTheme: (theme) => ({
+        baseBackgroundColor: '#fff',
+    })
+};
+
+export default tableConfig;
